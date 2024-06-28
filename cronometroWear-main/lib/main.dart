df03a7e6e867825b08c8e2ac7fcaf454e694aba8
MateoRaul2121 <mateoraul2121@gmail.com>
2024-06-28 12:39:45 -0600

    Update main.dart

--- cronometroWear-main/lib/main.dart
+++ cronometroWear-main/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CronometroWatch',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.compact,
